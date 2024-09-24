59f6a156d241c8106ef9699ccc99dab5b79ed51d
Progress with the component <deMiguel@newsaktuell.de>
2024-09-24 13:36:56 +0200

    Refactor code to update import path for inngest client

--- src/app/actions/stremeable.ts
+++ src/app/actions/stremeable.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { inngest } from "@/ingest/client";
+import { inngest } from "@/inngest/client";
 import { openai } from "@ai-sdk/openai";
 import { streamText } from "ai";
 import { createStreamableValue } from "ai/rsc";
