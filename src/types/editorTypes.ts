ac32fe100cc7d5e6ee79f36964ec0770e669a8c8
Progress with the component <deMiguel@newsaktuell.de>
2024-09-24 11:15:04 +0200

    Add typings for @editorjs/paragraph and @editorjs/embed modules

---
+++ src/types/editorTypes.ts
@@ -0,0 +1,15 @@
+declare module "@editorjs/paragraph" {
+  import { BlockTool } from "@editorjs/editorjs";
+
+  const Paragraph: BlockTool;
+
+  export default Paragraph;
+}
+
+declare module "@editorjs/embed" {
+  import { BlockTool } from "@editorjs/editorjs";
+
+  const Embed: BlockTool;
+
+  export default Embed;
+}
