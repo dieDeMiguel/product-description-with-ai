e92555d524439a6135dda27f9132e69117f2bc84
Progress with the component <deMiguel@newsaktuell.de>
2024-10-01 14:24:27 +0200

    Refactor code to remove @tanstack/react-query-devtools package and integrate it into RQProvider component

--- src/app/RQProvider.tsx
+++ src/app/RQProvider.tsx
@@ -1,48 +1,14 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ReactNode, Suspense, lazy, useEffect, useState } from "react";
+import { useState } from "react";
 
-const ReactQueryDevtoolsProduction = lazy(() =>
-  import("@tanstack/react-query-devtools/build/modern/production.js").then(
-    (d) => ({
-      default: d.ReactQueryDevtools,
-    })
-  )
-);
-
-interface Props {
-  children: ReactNode;
-}
-
-export default function QueryClientProviderComponent({ children }: Props) {
-  const [showDevtools, setShowDevtools] = useState(false);
-
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    window.toggleDevtools = () => setShowDevtools((old) => !old);
-  }, []);
-
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 1000 * 60 * 5,
-        retry: 2,
-        refetchOnWindowFocus: true,
-      },
-    },
-  });
-
+export default function RQProvider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-      {showDevtools && (
-        <Suspense fallback={null}>
-          <ReactQueryDevtoolsProduction />
-        </Suspense>
-      )}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
