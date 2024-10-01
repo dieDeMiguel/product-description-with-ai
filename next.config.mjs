c8c49a007e040b300ba3d0c039db2a2809ddb206
Progress with the component <deMiguel@newsaktuell.de>
2024-10-01 11:01:59 +0200

    Refactor code to add serverAction and streaming options

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  serverAction: true,
   images: {
     domains: ["t3xyzgvkkyktkixg.public.blob.vercel-storage.com"],
   },
--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -6,4 +6,5 @@ import { serve } from "inngest/next";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [throttleGeneratePressRelease, throttleGenerateImageCaption],
+  streaming: "allow",
 });
