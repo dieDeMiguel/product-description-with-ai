bc68db386e4b075c3e09a74062e00094f5bcdd56
Progress with the component <deMiguel@newsaktuell.de>
2024-10-02 10:02:16 +0200

    Refactor code to update table and column names in press-release page

--- src/db/index.ts
+++ src/db/index.ts
@@ -17,21 +17,22 @@ export async function setPressRelease(
   id: number,
   pressRelease: string
 ): Promise<void> {
-  await sql`UPDATE pressreleases_assets SET pressrelease=${pressRelease} WHERE id=${id}`;
+  await sql`UPDATE pressreleases_assets_old SET pressrelease=${pressRelease} WHERE id=${id}`;
 }
 
 export async function setPressReleaseCompleted(
   id: number,
   isPressReleaseGenerationFinished: boolean
 ): Promise<void> {
-  await sql`UPDATE pressreleases_assets SET pressrelease_completed=${isPressReleaseGenerationFinished} WHERE id=${id}`;
+  await sql`UPDATE pressreleases_assets_old SET pressrelease_completed=${isPressReleaseGenerationFinished} WHERE id=${id}`;
 }
 
 export async function getGeneratedPressRelease(
   id: number
 ): Promise<PressReleaseAsset> {
   try {
-    const result = await sql`SELECT * FROM pressreleases_assets WHERE id=${id}`;
+    const result =
+      await sql`SELECT * FROM pressreleases_assets_old WHERE id=${id}`;
     if (result.rows.length === 0) {
       throw new Error(`No press release found with id ${id}`);
     }
@@ -47,17 +48,17 @@ export async function setGeneratedPressRelease(
   pressRelease: string
 ): Promise<number> {
   const result =
-    await sql`INSERT INTO pressreleases_assets (pressrelease) VALUES (${pressRelease}) RETURNING id`;
+    await sql`INSERT INTO pressreleases_assets_old (pressrelease) VALUES (${pressRelease}) RETURNING id`;
   return result.rows[0].id;
 }
 export async function setKeywords(id: number, keywords: string): Promise<void> {
-  await sql`UPDATE pressreleases_assets SET keywords=${keywords} WHERE id=${id}`;
+  await sql`UPDATE pressreleases_assets_old SET keywords=${keywords} WHERE id=${id}`;
 }
 
 export async function getGeneratedKeywords(id: number): Promise<string> {
   try {
     const result =
-      await sql`SELECT keywords FROM pressreleases_assets WHERE id=${id}`;
+      await sql`SELECT keywords FROM pressreleases_assets_old WHERE id=${id}`;
     if (result.rows.length === 0) {
       throw new Error(`No press release found with id ${id}`);
     }
@@ -72,23 +73,23 @@ export async function setKeywordsCompleted(
   id: number,
   isKeywordGenerationFinished: boolean
 ): Promise<void> {
-  await sql`UPDATE pressreleases_assets SET keywords_completed=${isKeywordGenerationFinished} WHERE id=${id}`;
+  await sql`UPDATE pressreleases_assets_old SET keywords_completed=${isKeywordGenerationFinished} WHERE id=${id}`;
 }
 
 export async function upsertImage(image: string, id: string): Promise<void> {
-  await sql`UPDATE pressreleases_assets SET image=${image} WHERE id=${id}`;
+  await sql`UPDATE pressreleases_assets_old SET image=${image} WHERE id=${id}`;
 }
 
 export async function setImageCaption(
   id: number,
   caption: string
 ): Promise<void> {
-  await sql`UPDATE pressreleases_assets SET image_caption=${caption} WHERE id=${id}`;
+  await sql`UPDATE pressreleases_assets_old SET image_caption=${caption} WHERE id=${id}`;
 }
 
 export async function setImageCaptionCompleted(
   id: number,
   captionCompleted: boolean
 ): Promise<void> {
-  await sql`UPDATE pressreleases_assets SET image_caption_completed=${captionCompleted} WHERE id=${id}`;
+  await sql`UPDATE pressreleases_assets_old SET image_caption_completed=${captionCompleted} WHERE id=${id}`;
 }
