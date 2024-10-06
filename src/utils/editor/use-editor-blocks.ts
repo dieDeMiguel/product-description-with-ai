cb07bb55c45549c3e31fdd4fd4fd1f8c49ef82a5
Progress with the component <deMiguel@newsaktuell.de>
2024-10-06 18:57:19 +0200

    Refactor useEditorBlocks to return Promise<OutputBlockData[]>

--- src/utils/editor/use-editor-blocks.ts
+++ src/utils/editor/use-editor-blocks.ts
@@ -1,13 +1,15 @@
 import { PressReleaseAsset } from "@/db";
-import { BlockAPI } from "@editorjs/editorjs";
+import { OutputBlockData } from "@editorjs/editorjs";
 import { useMemo } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-const useEditorBlocks = (data: PressReleaseAsset | null | undefined) =>
+const useEditorBlocks = (
+  data: PressReleaseAsset | null | undefined
+): Promise<OutputBlockData[]> =>
   useMemo(() => {
     if (data?.pressrelease_body) {
       const parsedBody = JSON.parse(JSON.parse(data.pressrelease_body));
-      return parsedBody?.blocks?.map((block: BlockAPI) => ({
+      return parsedBody?.blocks?.map((block: OutputBlockData) => ({
         ...block,
         id: block.id || uuidv4(),
       }));
